49a5ee2695568b93618e91ba16631b97bb67049b
Muhammed Karkas <muhammed.karkas@gmail.com>
2023-03-26 07:15:57 +0300

    classes done

---
+++ src/Course.java
@@ -0,0 +1,16 @@
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    public Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+    }
+
+}
+
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    public Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        this.isPass = false;
+    }
+
+}
