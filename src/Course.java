f644891fa93722d909f4518d126722c8f428af6b
Muhammed Karkas <muhammed.karkas@gmail.com>
2023-03-26 07:19:54 +0300

    course class methods done

--- src/Course.java
+++ src/Course.java
@@ -12,5 +12,22 @@ public class Course {
         this.note = 0;
     }
 
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+
 }
 
